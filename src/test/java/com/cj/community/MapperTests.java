e77b9b06d2d07dac6053234dc0deb73f91846610
cj <453272889@qq.com>
2020-05-29 15:22:47 +0800

    更新提交

--- src/test/java/com/cj/community/MapperTests.java
+++ src/test/java/com/cj/community/MapperTests.java
@@ -1,8 +1,12 @@
 package com.cj.community;
 
 import com.cj.community.dao.DiscussPostMapper;
+import com.cj.community.dao.LoginTicketMapper;
+import com.cj.community.dao.MessageMapper;
 import com.cj.community.dao.UserMapper;
 import com.cj.community.entity.DiscussPost;
+import com.cj.community.entity.LoginTicket;
+import com.cj.community.entity.Message;
 import com.cj.community.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.junit.Test;
@@ -12,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.sql.SQLOutput;
 import java.util.Date;
 import java.util.List;
 
@@ -25,6 +30,12 @@ public class MapperTests {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
+
+    @Autowired
+    private MessageMapper messageMapper;
 
     @Test
     public void testSelectUser(){
@@ -67,12 +78,55 @@ public class MapperTests {
 
     @Test
     public void testSelectPosts(){
-        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149,0,10);
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149,0,10, 0);
         for (DiscussPost post : list){
             System.out.println(post);
         }
 
         int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
+    }
+
+    @Test
+    public void testInsertLoginTicket(){
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket(){
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc", 1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+    }
+
+    @Test
+    public void testSelectLetters(){
+        List<Message> list = messageMapper.selectConversations(111, 0, 20);
+        for (Message message : list){
+            System.out.println(message);
+        }
+
+        int count = messageMapper.selectConversationCount(111);
+        System.out.println(count);
+
+        list = messageMapper.selectLetters("111_112", 0, 10);
+        for (Message message : list){
+            System.out.println(message);
+        }
+
+        count = messageMapper.selectLetterCount("111_112");
+        System.out.println(count);
+
+        count = messageMapper.selectLetterUnreadCount(131, "111_131");
+        System.out.println(count);
     }
 }
